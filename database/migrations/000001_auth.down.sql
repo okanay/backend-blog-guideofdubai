2416177cf8f03cb72de356e68992b6808fa07c6d
Okan Ay <okanay@Okan-MacBook-Pro-2.local>
2025-04-13 09:27:21 +0300

    drop

--- database/migrations/000001_auth.down.sql
+++ database/migrations/000001_auth.down.sql
@@ -1,19 +1,7 @@
 -- İndeksleri kaldır
-DROP INDEX IF EXISTS idx_refresh_tokens_user_email;
-
-DROP INDEX IF EXISTS idx_refresh_tokens_user_username;
-
-DROP INDEX IF EXISTS idx_refresh_tokens_user_id_is_revoked;
-
-DROP INDEX IF EXISTS idx_refresh_tokens_user_id;
-
 DROP INDEX IF EXISTS idx_refresh_tokens_token;
 
-DROP INDEX IF EXISTS idx_users_status;
-
 DROP INDEX IF EXISTS idx_users_username;
-
-DROP INDEX IF EXISTS idx_users_email;
 
 -- Tabloları kaldır
 DROP TABLE IF EXISTS refresh_tokens;
